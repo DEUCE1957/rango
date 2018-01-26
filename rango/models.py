5e2fa2e1bcd8501fa31e2231e19822aa33fa15e8
Xavier Weiss <2246306W@student.gla.ac.uk>
2018-01-26 10:09:33 +0000

    NewMachine <Chapter 5.5 commit

--- rango/models.py
+++ rango/models.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Category(models.Model):
+	name = models.CharField(max_length=128,unique=True)
+	
+	def _str_(self):
+		return self.name
+		
+class Page(models.Model):
+	category = models.ForeignKey(Category)
+	title = models.CharField(max_length=128)
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+	
+	def _str_(self):
+		return self.title
 # Create your models here.
