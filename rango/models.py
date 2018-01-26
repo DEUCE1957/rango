e625f06c9606453ce2147ebfe980d11342e76713
Xavier Weiss <2246306W@student.gla.ac.uk>
2018-01-26 10:56:43 +0000

    New Machine Chapter 5.6 Commit

--- rango/models.py
+++ rango/models.py
@@ -6,7 +6,9 @@ from django.db import models
 class Category(models.Model):
 	name = models.CharField(max_length=128,unique=True)
 	
-	def _str_(self):
+	class Meta:
+		verbose_name_plural = 'Categories'
+	def __str__(self):
 		return self.name
 		
 class Page(models.Model):
@@ -15,6 +17,6 @@ class Page(models.Model):
 	url = models.URLField()
 	views = models.IntegerField(default=0)
 	
-	def _str_(self):
+	def __str__(self):
 		return self.title
 # Create your models here.
